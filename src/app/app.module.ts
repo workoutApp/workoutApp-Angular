186d19306e86856351c00d23cd8019188ebc6f0c
Benjamin Allred <benjaminallred@Benjamins-MacBook-Air.local>
2018-12-28 16:36:01 -0500

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
