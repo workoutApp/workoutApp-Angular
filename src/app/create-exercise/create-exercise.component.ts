486c508fb928908099aeb8c548c9013541f58002
DaveKeith <d.keith78@yahoo.com>
2018-12-31 07:41:46 -0500

    created create-exercise, uncommented parts of exercise-list that we'll need

---
+++ src/app/create-exercise/create-exercise.component.ts
@@ -0,0 +1,89 @@
+import { Response } from '@angular/http';
+import { ExerciseService } from '../services/exercise.services';
+import Exercise from '../models/exercise.models';
+import { Component, OnInit } from '@angular/core';
+// import { faCoffee } from 'font-awesome';
+// import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+@Component({
+  selector: 'app-create-exercise',
+  templateUrl: './create-exercise.component.html',
+  styleUrls: ['./create-exercise.component.scss']
+})
+export class CreateExerciseComponent implements OnInit {
+
+  // constructor() { }
+
+  // ngOnInit() {
+  // }
+constructor(
+  // Private todoservice will be injected into the component by Angular Dependency Injector
+  private exerciseService: ExerciseService
+) { }
+
+// Declaring the new exercise Object and initilizing it
+public newExercise: Exercise = new Exercise();
+
+// An Empty list for the visible exercise list
+exercisesList: Exercise[];
+editExercise: Exercise[] = [];
+
+
+ngOnInit(): void {
+
+  // At component initialization the
+  this.exerciseService.getExercises()
+    .subscribe(exercises => {
+      // assign the todolist property to the proper http response
+      this.exercisesList = exercises;
+      console.log(exercises);
+    });
+}
+
+// This method will get called on Create button event
+create() {
+  this.exerciseService.createExercise(this.newExercise)
+    .subscribe((res) => {
+      this.exercisesList.push(res.data);
+      this.newExercise = new Exercise();
+    });
+}
+
+editExcercise(exercise: Exercise) {
+  console.log(exercise);
+   if (this.exercisesList.includes(exercise)) {
+    if (!this.editExercise.includes(exercise)) {
+      this.editExercise.push(exercise);
+    } else {
+      this.editExercise.splice(this.editExercise.indexOf(exercise), 1);
+      this.exerciseService.editExercise(exercise).subscribe(res => {
+        console.log('Update Succesful');
+       }, err => {
+          console.error('Update Unsuccesful');
+        });
+      }
+    }
+  }
+
+  doneExcercise(exercise: Exercise) {
+    exercise.complete = true;
+    this.exerciseService.editExercise(exercise).subscribe(res => {
+      console.log('Update Succesful');
+    }, err => {
+      console.error('Update Unsuccesful');
+    });
+  }
+
+  submitExcercise(event, exercise: Exercise) {
+    if (event.keyCode === 13) {
+      this.editExcercise(exercise);
+    }
+  }
+
+  deleteExcercise(exercise: Exercise) {
+    this.exerciseService.deleteExercise(exercise._id).subscribe(res => {
+      this.exercisesList.splice(this.exercisesList.indexOf(exercise), 1);
+    });
+  }
+
+}
--- src/app/exercise-list/exercise-list.component.ts
+++ src/app/exercise-list/exercise-list.component.ts
@@ -14,18 +14,18 @@ export class ExerciseListComponent implements OnInit {
 
     // constructor() { }
 
-    // ngOnInit() {}
+  //  ngOnInit() {}
   constructor(
     // Private todoservice will be injected into the component by Angular Dependency Injector
     private exerciseService: ExerciseService;
   ) { }
 
-  // Declaring the new exercise Object and initilizing it
-  public newExercise: Exercise = new Exercise();
+  // // Declaring the new exercise Object and initilizing it
+  // public newExercise: Exercise = new Exercise();
 
-  // An Empty list for the visible exercise list
+  // // An Empty list for the visible exercise list
   exercisesList: Exercise[];
-  editExercise: Exercise[] = [];
+  // editExercise: Exercise[] = [];
 
 
   ngOnInit(): void {
@@ -39,49 +39,49 @@ export class ExerciseListComponent implements OnInit {
       });
   }
 
-  // This method will get called on Create button event
-  create() {
-    this.exerciseService.createExercise(this.newExercise)
-      .subscribe((res) => {
-        this.exercisesList.push(res.data);
-        this.newExercise = new Exercise();
-      });
-  }
+  // // This method will get called on Create button event
+  // create() {
+  //   this.exerciseService.createExercise(this.newExercise)
+  //     .subscribe((res) => {
+  //       this.exercisesList.push(res.data);
+  //       this.newExercise = new Exercise();
+  //     });
+  // }
 
-  editTodo(exercise: Exercise) {
-    console.log(exercise);
-     if (this.exercisesList.includes(exercise)) {
-      if (!this.editExercise.includes(exercise)) {
-        this.editExercise.push(exercise);
-      } else {
-        this.editExercise.splice(this.editExercise.indexOf(exercise), 1);
-        this.exerciseService.editExercise(exercise).subscribe(res => {
-          console.log('Update Succesful');
-         }, err => {
-            console.error('Update Unsuccesful');
-          });
-        }
-      }
-    }
+  // editTodo(exercise: Exercise) {
+  //   console.log(exercise);
+  //    if (this.exercisesList.includes(exercise)) {
+  //     if (!this.editExercise.includes(exercise)) {
+  //       this.editExercise.push(exercise);
+  //     } else {
+  //       this.editExercise.splice(this.editExercise.indexOf(exercise), 1);
+  //       this.exerciseService.editExercise(exercise).subscribe(res => {
+  //         console.log('Update Succesful');
+  //        }, err => {
+  //           console.error('Update Unsuccesful');
+  //         });
+  //       }
+  //     }
+  //   }
 
-    doneTodo(exercise: Exercise) {
-      exercise.status = 'Done';
-      this.exerciseService.editExercise(texercise.subscribe(res => {
-        console.log('Update Succesful');
-      }, err => {
-        console.error('Update Unsuccesful');
-      });
-    }
+  //   doneTodo(exercise: Exercise) {
+  //     exercise.status = 'Done';
+  //     this.exerciseService.editExercise(texercise.subscribe(res => {
+  //       console.log('Update Succesful');
+  //     }, err => {
+  //       console.error('Update Unsuccesful');
+  //     });
+  //   }
 
-    submitTodo(event, exercise: Exercise) {
-      if (event.keyCode === 13) {
-        this.editTodo(exercise);
-      }
-    }
+  //   submitTodo(event, exercise: Exercise) {
+  //     if (event.keyCode === 13) {
+  //       this.editTodo(exercise);
+  //     }
+  //   }
 
-    deleteTodo(exercise: Exercise) {
-      this.exerciseService.deleteExercise(exercise._id).subscribe(res => {
-        this.exercisesList.splice(this.exercisesList.indexOf(exercise), 1);
-      });
-    }
+  //   deleteTodo(exercise: Exercise) {
+  //     this.exerciseService.deleteExercise(exercise._id).subscribe(res => {
+  //       this.exercisesList.splice(this.exercisesList.indexOf(exercise), 1);
+  //     });
+  //   }
 }
